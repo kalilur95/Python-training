b498eb68fe8a0da49df098570cf96d9d60c944b7
kalilur95 <kalilurrahmanr@outlook.com>
2016-11-19 18:42:38 +0530

    First 10 tasks added

---
+++ python_assignment_1/task9.py
@@ -0,0 +1,18 @@
+#  Program to Add, Subtract, Multiply and Divide complex number
+
+c1=complex(input())	#Enter complex number in real+imag(j) format with out paranthesis
+c2=complex(input())
+
+#Complex addition
+print(c1+c2)
+
+#Complex Subtract
+print(c1-c2)
+
+#Complex Multiply
+print(c1*c2)
+
+#Complex Divide
+print(c1/c2)
+
+
